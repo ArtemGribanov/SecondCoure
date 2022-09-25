e29764569e9204973f2aa8065ccf2851dad99732
Artem <artemsypestar1@gmail.com>
2022-09-25 15:57:33 +0300

    add some piece of shit

--- AISD4/ConsoleApplication1/ConsoleApplication1.cpp
+++ AISD4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 int main()
 {
-    
+    int a,n=1,k=1,check=0;
+    cin >> a;
+	while (n<=a)
+	{
+		a=a - n;
+		k++;
+		if (a>=n)
+		{
+			cout << n<<" ";
+		}
+		else
+		{
+			cout << n<<" ";
+		}
+		n++;
+		
+	}
 }
 
