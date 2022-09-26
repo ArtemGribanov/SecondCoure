230a4746a95c4fb39a3004eebcac6619a888eb02
Artem <artemsypestar1@gmail.com>
2022-09-26 12:03:21 +0300

    somthing wrong

--- AISD4/ConsoleApplication1/ConsoleApplication1.cpp
+++ AISD4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,23 +1,69 @@
 ﻿#include <iostream>
-using namespace std;
-int main()
+
+bool PrintFragments(unsigned num, unsigned max, unsigned& nTry, unsigned lvl = 0);
+
+bool PrintFragmentsImpl(unsigned num, unsigned n, unsigned max,
+    unsigned& nTry, unsigned lvl)
 {
-    int a,n=1,k=1,check=0;
-    cin >> a;
-	while (n<=a)
-	{
-		a=a - n;
-		k++;
-		if (a>=n)
-		{
-			cout << n<<" ";
-		}
-		else
-		{
-			cout << n<<" ";
-		}
-		n++;
-		
-	}
+    if (!n)
+        return false;
+
+    if (n > max)
+        return PrintFragmentsImpl(num, n - 1, max, nTry, lvl);
+
+    bool found = false;
+
+    if (n == num)
+    {
+        if (!--nTry)
+            found = true;
+    }
+    else if (PrintFragments(num - n, n, nTry, lvl + 1))
+    {
+        found = true;
+    }
+
+    if (found)
+    {
+        std::cout << n << " ";
+
+        if (!lvl)
+            std::cout << "\n";
+
+        return true;
+    }
+
+    if (--n)
+        return PrintFragmentsImpl(num, n, max, nTry, lvl);
+
+    return false;
 }
 
+bool PrintFragments(unsigned num, unsigned max, unsigned& nTry, unsigned lvl)
+{
+    return PrintFragmentsImpl(num, num, max, nTry, lvl);
+}
+
+unsigned PrintNumFragmentsImpl(unsigned num, unsigned nTry)
+{
+    unsigned n = nTry;
+    if (PrintFragments(num, num, n))
+        return PrintNumFragmentsImpl(num, nTry + 1);
+    else
+        return nTry - 1;
+}
+
+void PrintNumFragmentsInfo(unsigned num)
+{
+    unsigned counter = PrintNumFragmentsImpl(num, 1);
+
+    std::cout << "total amounts of fragments is : " << counter << "\n";
+}
+
+int main()
+{
+    unsigned num = 0;
+    std::cin >> num;
+
+    PrintNumFragmentsInfo(num);
+}
