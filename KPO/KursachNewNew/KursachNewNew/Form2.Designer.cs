33d1c5bb6389f202480e6e3d202843cf027fe667
Artem <artemsypestar1@gmail.com>
2022-12-11 00:28:40 +0300

    add foe kursach

---
+++ KPO/KursachNewNew/KursachNewNew/Form2.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace KursachNewNew
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InputName = new System.Windows.Forms.TextBox();
+            this.ShowName = new System.Windows.Forms.Label();
+            this.ShowScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(47, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label2.Location = new System.Drawing.Point(252, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(132, 540);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputName
+            // 
+            this.InputName.Location = new System.Drawing.Point(121, 483);
+            this.InputName.Name = "InputName";
+            this.InputName.Size = new System.Drawing.Size(100, 23);
+            this.InputName.TabIndex = 3;
+            // 
+            // ShowName
+            // 
+            this.ShowName.AutoSize = true;
+            this.ShowName.Location = new System.Drawing.Point(47, 64);
+            this.ShowName.Name = "ShowName";
+            this.ShowName.Size = new System.Drawing.Size(0, 15);
+            this.ShowName.TabIndex = 4;
+            // 
+            // ShowScore
+            // 
+            this.ShowScore.AutoSize = true;
+            this.ShowScore.Location = new System.Drawing.Point(252, 64);
+            this.ShowScore.Name = "ShowScore";
+            this.ShowScore.Size = new System.Drawing.Size(0, 15);
+            this.ShowScore.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 455);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter your name";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(342, 598);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ShowScore);
+            this.Controls.Add(this.ShowName);
+            this.Controls.Add(this.InputName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox InputName;
+        private Label ShowName;
+        private Label ShowScore;
+        private Label label3;
+    }
+}
---
+++ KPO/KursachNewNew/KursachNewNew/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KursachNewNew
+{
+    public partial class Form2 : Form
+    {
+        string path = @"D:\GIT-Repository\SecondCoure\KPO\KursachNewNew\FilesForKursach\Name.txt";
+        string pathscore = @"D:\GIT-Repository\SecondCoure\KPO\KursachNewNew\FilesForKursach\Score.txt";
+        
+        public Form2(string score)
+        {
+            InitializeComponent();
+           
+            string name = File.ReadAllText(path); 
+            ShowName.Text += name ;
+            string scr = score.ToString()+"\n" ;
+            File.AppendAllText(pathscore, scr);
+            string str1 = File.ReadAllText(pathscore);
+            ShowScore.Text += str1;
+            InputName.Visible = true;
+        }
+       
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            ShowName.Text = " ";
+            string name = InputName.Text+"\n";
+            File.AppendAllText(path, name);
+            string str = File.ReadAllText(path);
+            ShowName.Text += str ;
+            InputName.Text = "";
+            InputName.Visible = false;
+            
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+       
+    }
+}
